efdcfb11ec477c2c4d32fafff1ad1ff7af543242
Ganesh <ganesh.birajdar.1930@gmail.com>
2023-02-16 19:28:37 +0530

    ts

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+// let username = "Ganesh";
+// console.log(username);
+
+// let a: number = 12;
+// // let b: string = '6'
+// let b: number = 6
+// let c: number = 2
+
+// console.log(a / b)
+
+// console.log(c * b)
+
+
+let myName: string = "Ganesh";
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: any;
+
+// myName = 45; // can't do this
+myName = "Birajdar";
+meaningOfLife = 7;
+isLoading = true;
+album = "starboy";
