41e21d8b4216abf99ecbfac80c3c8daa79628193
Ganesh <ganesh.birajdar.1930@gmail.com>
2023-02-17 00:33:52 +0530

    generic

--- build/js/indexSignature.js
+++ build/js/indexSignature.js
@@ -1,14 +1,10 @@
 "use strict";
 // Index signatures -- > to access obj props dynamically --when u dont't knw the props of obj
-// interface TransactionObj{ 
-//     Pizza: number,
-//     Books: number,
-//     Job: number
-// }
 const todaysTransactions = {
     Pizza: -10,
     Books: -5,
-    Job: 50
+    Job: 50,
+    // Dave: 223
 };
 console.log(todaysTransactions.Pizza);
 console.log(todaysTransactions['Pizza']);
@@ -24,3 +20,27 @@ const todaysNet = (transactions) => {
 // console.log(todaysTransactions)
 // todaysTransactions.Pizza = 79; can't do this
 console.log(todaysTransactions["Dave"]);
+const student = {
+    name: "Doug",
+    GPA: 5.6,
+    classes: [133, 534]
+};
+// console.log(student.test)
+for (const key in student) {
+    console.log(`${key}: ${student[key]}`);
+}
+Object.keys(student).map(key =  > {
+    console, : .log(student[key])
+});
+const logStudentKey = (student, key) => {
+    console.log(`Student ${key}: ${student[key]}`);
+};
+logStudentKey(student, 'name');
+// const montlyIncomes: Incomes = {
+//     salary: 500,
+//     bonus: 100,
+//     sideHustile : 250
+// }
+for (const revenue in montlyIncomes) {
+    console.log(montlyIncomes[revenue]);
+}
--- src/indexSignature.ts
+++ src/indexSignature.ts
@@ -1,19 +1,21 @@
 // Index signatures -- > to access obj props dynamically --when u dont't knw the props of obj
 
 
-interface TransactionObj{
-    readonly [index: string]: number
-}
-// interface TransactionObj{ 
-//     Pizza: number,
-//     Books: number,
-//     Job: number
+// interface TransactionObj{
+//     readonly [index: string]: number
 // }
+interface TransactionObj{ 
+    readonly [index: string]: number
+    Pizza: number,
+    Books: number,
+    Job: number
+}
 
 const todaysTransactions: TransactionObj = {
     Pizza: -10,
     Books: -5,
-    Job: 50
+    Job: 50,
+    // Dave: 223
 }
 
 console.log(todaysTransactions.Pizza)
@@ -35,3 +37,57 @@ const todaysNet = (transactions: TransactionObj): number => {
 // todaysTransactions.Pizza = 79; can't do this
 
 console.log(todaysTransactions["Dave"])
+
+
+// ------------
+
+interface Student{
+    // [key: string]: string | number | number[] | undefined
+    name: string,
+    GPA: number,
+    classes?: number[]
+}
+
+const student: Student= {
+    name: "Doug",
+    GPA: 5.6,
+    classes: [133,534]
+}
+
+// console.log(student.test)
+
+for(const key in student){
+    console.log(`${key}: ${student[key as keyof Student]}`)
+}
+
+Object.keys(student).map( key = > {
+    console.log(student[key as keyof typeof student])
+})
+
+const logStudentKey = (student: Student, key: keyof Student): void => {
+    console.log(`Student ${key}: ${student[key]}`)
+}
+
+logStudentKey(student,'name');
+
+/////////////
+
+// interface Incomes{
+//     [key: string] : number
+
+// }
+
+type Streams = 'salary' | 'bonus' | 'sidehustle'
+
+//provide a literal type
+type Incomes = Record<Streams, number | string>
+
+// const montlyIncomes: Incomes = {
+//     salary: 500,
+//     bonus: 100,
+//     sideHustile : 250
+// }
+
+for(const revenue in montlyIncomes){
+    console.log(montlyIncomes[revenue as keyof Incomes])
+}
